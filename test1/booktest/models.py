4954e2193aa348e19fd92ebef7b1f65f6fae98e0
qqqqq <1046082542@qq.com>
2019-06-04 20:22:51 +0800

    django命令操作

---
+++ test1/booktest/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class BookInfo(models.Model):
+    title = models.CharField(max_length=20)
+    pub_date = models.DateTimeField()
+
+class HeroInfo(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.BooleanField(default=True)
+    content = models.CharField(max_length=100)
+
+    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
