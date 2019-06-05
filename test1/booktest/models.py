d571a6740232df447d602a937aa874c4e749321a
qqqqq <1046082542@qq.com>
2019-06-05 21:18:32 +0800

    MVT联合操作

--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-
-# Create your models here.
 class BookInfo(models.Model):
-    title = models.CharField(max_length=20)
-    pub_date = models.DateTimeField()
+    title=models.CharField(max_length=20)
+    pub_date=models.DateField()
+    def __str__(self):
+        return self.title
 
 class HeroInfo(models.Model):
-    name = models.CharField(max_length=20)
-    gender = models.BooleanField(default=True)
-    content = models.CharField(max_length=100)
+    name=models.CharField(max_length=20)
+    gender=models.BooleanField(default=True)
+    content=models.CharField(max_length=100)
 
-    book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    book=models.ForeignKey(BookInfo,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+# Create your models here.
--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import BookInfo,HeroInfo
 # Create your views here.
+def index(req):
+    temp = loader.get_template('booktest/index.html')
+    res = temp.render({})
+    return HttpResponse(res)
+
+def list(req):
+    books=BookInfo.objects.all()
+    print(books)
+
+    temp = loader.get_template('booktest/list.html')
+    res = temp.render({"books":books})
+    return HttpResponse(res)
+
+def detail(req,id):
+    book=BookInfo.objects.get(pk=id)
+
+    temp = loader.get_template('booktest/detail.html')
+    res = temp.render({"book":book})
+    return HttpResponse(res)
