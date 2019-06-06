e70053cf1c1aeb4fdc5d01c7e5d462fac4eda3f8
qqqqq <1046082542@qq.com>
2019-06-06 10:26:18 +0800

    系统二上传

---
+++ env2/test1/booktest/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import BookInfo,HeroInfo
+# Create your views here.
+def index(req):
+    temp = loader.get_template('booktest/index.html')
+    res = temp.render({ "username":"dsl" })
+    return HttpResponse(res)
+    # return render(req,'booktest/index.html',)
+
+def list(req):
+    books=BookInfo.objects.all()
+
+    temp = loader.get_template('booktest/list.html')
+    res = temp.render({"books":books})
+    return HttpResponse(res)
+    # return render(req,'booktest/list.html',{"books":books})
+
+def detail(req,id):
+    book=BookInfo.objects.get(pk=id)
+
+    temp = loader.get_template('booktest/detail.html')
+    res = temp.render({"book":book})
+    return HttpResponse(res)
+    # return render(req,'booktest/detail.html',{"book":book})
