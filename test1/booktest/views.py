19791c60399f3705378a3811491e76e2331c5c3c
qqqqq <1046082542@qq.com>
2019-06-18 15:40:12 +0800

    等待上传虚拟环境

--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -4,21 +4,23 @@ from django.template import loader
 from .models import BookInfo,HeroInfo
 # Create your views here.
 def index(req):
-    temp = loader.get_template('booktest/index.html')
-    res = temp.render({})
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/index.html')
+    # res = temp.render({ "username":"dsl" })
+    # return HttpResponse(res)
+    return render(req,'booktest/index.html',)
 
 def list(req):
     books=BookInfo.objects.all()
-    print(books)
 
-    temp = loader.get_template('booktest/list.html')
-    res = temp.render({"books":books})
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/list.html')
+    # res = temp.render({"books":books})
+    # return HttpResponse(res)
+    return render(req,'booktest/list.html',{"books":books})
 
 def detail(req,id):
     book=BookInfo.objects.get(pk=id)
 
-    temp = loader.get_template('booktest/detail.html')
-    res = temp.render({"book":book})
-    return HttpResponse(res)
+    # temp = loader.get_template('booktest/detail.html')
+    # res = temp.render({"book":book})
+    # return HttpResponse(res)
+    return render(req,'booktest/detail.html',{"book":book})
